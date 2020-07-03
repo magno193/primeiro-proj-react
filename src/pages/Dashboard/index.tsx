8fa6fe111efd9989dcebadc1a32f460655189626
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-07-03 12:25:10 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,57 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
 import { Title, Form, Repositories } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="github-logo" />
-    <Title>Explore repositórios no Github</Title>
-    <Form action="">
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars3.githubusercontent.com/u/34192221?s=460&u=c34d9e441820b91d7f12753efd42cf27b3e8e5a7&v=4"
-          alt="Alexandre Ferreira"
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="github-logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
         />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy high scallabe ReactJS & React Native forms!</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars3.githubusercontent.com/u/34192221?s=460&u=c34d9e441820b91d7f12753efd42cf27b3e8e5a7&v=4"
-          alt="Alexandre Ferreira"
-        />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy high scallabe ReactJS & React Native forms!</p>
-        </div>
+      <Repositories>
+        {repositories.map(repository => (
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img
-          src="https://avatars3.githubusercontent.com/u/34192221?s=460&u=c34d9e441820b91d7f12753efd42cf27b3e8e5a7&v=4"
-          alt="Alexandre Ferreira"
-        />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy high scallabe ReactJS & React Native forms!</p>
-        </div>
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+            <FiChevronRight size={20} />
+          </a>
+
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
